354c227ae2af5b5c8b6e13ca9973f9a38f3bdcd6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-29 16:35:09 +0200

    main barebone

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,3 +1,18 @@
 <script></script>
-<template></template>
-<style></style>
+<template>
+    <section id="main">
+        <div class="container">
+            <h2>Content goes here</h2>
+        </div>
+    </section>
+</template>
+<style scoped>
+#main {
+    background-color: #1C1C1C;
+}
+
+h2 {
+    color: white;
+    padding: 30px 0;
+}
+</style>
