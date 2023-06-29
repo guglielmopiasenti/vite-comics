4c368b297b35cfbe94060b47346682c3928d7377
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-29 17:44:33 +0200

    creating footer array + structure barebone

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,54 @@
-<script></script>
-<template></template>
-<style></style>
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    img: 'src/assets/img/buy-comics-digital-comics.png',
+                    title: 'DIGITAL COMICS',
+                },
+                {
+                    img: 'src/assets/img/buy-comics-merchandise.png',
+                    title: 'DC MERCHANDISE',
+                },
+                {
+                    img: 'src/assets/img/buy-comics-subscriptions.png',
+                    title: 'SUBSCRIPTION',
+                },
+                {
+                    img: 'src/assets/img/buy-comics-shop-locator.png',
+                    title: 'COMIC SHOP LOCATOR',
+                },
+                {
+                    img: 'src/assets/img/buy-dc-power-visa.svg',
+                    title: 'DC POWER VISA',
+                },
+            ],
+        }
+    }
+}
+</script>
+
+<template>
+    <section id="upper-footer">
+        <div class="container"></div>
+    </section>
+    <section id="main-footer">
+        <div class="container"></div>
+    </section>
+    <section id="lower-footer">
+        <div class="container"></div>
+    </section>
+</template>
+
+<style>
+#upper-footer {
+    background-color: #0782F9;
+}
+
+#main-footer {}
+
+#lower-footer {
+    background-color: #303030;
+}
+</style>
