acc12f49616a5ab273724eead872a50801f46304
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-29 15:13:56 +0200

    importing data onto appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,63 @@
 <script>
+export default {
+    data() {
+        return {
+            headerSections: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ],
+
+        }
+    }
+}
 </script>
 <template></template>
 <style></style>
