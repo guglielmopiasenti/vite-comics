7433ab30a36b55949104430ba67660f4f1c52865
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-03 08:54:46 +0200

    importing bold font weight

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import "@fontsource/open-sans";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/open-sans/800.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
