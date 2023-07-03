2da8d0b1b20c6e152f3849e2f98057dffd1e9262
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-03 08:45:06 +0200

    adding current series badge

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,7 @@ export default {
 </script>
 <template>
     <section id="main">
+        <h1 class="title">CURRENT SERIES</h1>
         <AppJumbotron />
         <div class="container">
             <AppComicCard :comicBooks="comicBooks" />
@@ -18,7 +19,24 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use 'src/assets/scss/_var.scss' as *;
+
 #main {
     background-color: #1C1C1C;
+    position: relative;
+    z-index: 0;
+}
+
+.title {
+    position: absolute;
+    top: 35%;
+    left: 16%;
+    transform: translate(-50%, -50%);
+    background-color: $blue;
+    color: white;
+    padding: 10px 20px;
+    font-size: 20px;
+    font-weight: bold;
+    z-index: 1;
 }
 </style>
